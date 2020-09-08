2739437d2e1bf6cf69d9bacc28b276eac6567e39
ABINASH-KUMAR-ctrl <69797730+ABINASH-KUMAR-ctrl@users.noreply.github.com>
2020-09-08 08:33:20 +0530

    Create create_array_with_numpy.py
    hghhahhhiojh

---
+++ create_array_with_numpy.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+#creating array using numpy
+aklist=[[1,2,3],[4,5,1],[5,5,6]]
+ARRAY=np.array(aklist)
+print("ARRAY=\n",ARRAY)
+
+
+#find the dimension of array
+print("THE DIMEMSION OF ARRAY IS ",ARRAY.shape)
+
+
+#create number list from 0 to 30(excluding 30) with a gap of 2
+n=np.arange(0,30,2)
+print("LIST FROM 0 to 30 with gap of two",n)
+
+#reshape your generated array
+n=n.reshape(5,3)
+print("reshaped array is \n",n)
+
+
+#resize array
+a = np.arange(6)
+a = np.resize(a, (2,3))
+print("resized array is \n",a)
+
+
+#create 9 numbers from 0 to 4 keeping equal gaps between numbers
+o=np.linspace(0,4,9)
+print("creating 9 numbers from 0 to 4 keeping equal gaps between numbers\n",o)
+
+print("2_D array with ones on the diagnol and zeros elsewhere\n",np.eye(3))
+print("find the diagnol ",np.diag(ARRAY))
+print(" Repeat elements of an array using repeat.",np.array([1,2,3,4]*4))
